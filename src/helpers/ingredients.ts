ada95920800121873194eb553780c7ee15c7ce45
jczujko <jarek.czujko@hotmail.com>
2023-02-19 21:53:22 +0100

    Updates

--- src/helpers/ingredients.ts
+++
@@ -1,8 +0,0 @@
-import { type Pizza } from '../utils/types'
-
-export const getUniqueIngredientsFromPizzaList = (pizzaList: Pizza[]): string[] => {
-  const ingredients = pizzaList.map((pizza: Pizza) => pizza.ingredients).flat()
-  const normalizedIngredients = ingredients.map((ingredient: string) => ingredient.trim().toLowerCase())
-  const uniqueIngredients = new Set<string>(normalizedIngredients)
-  return Array.from(uniqueIngredients)
-}
